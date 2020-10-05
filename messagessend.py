5e2ea367dd0887b53b2fd396c84ab6145cdb5ae6
alebovic <asaflbvc@gmail.com>
2020-10-05 15:45:14 -0400

    Update messagessend.py

--- messagessend.py
+++ messagessend.py
@@ -1,8 +1,11 @@
 import subprocess
 import csv
+import requests
 
+onlinecontacts = requests.get('https://raw.githubusercontent.com/alebovic/textbank/master/contacts.csv').text
+bashfile = requests.get('https://raw.githubusercontent.com/alebovic/textbank/master/messagesapp.sh').text
 
-with open('https://raw.githubusercontent.com/alebovic/textbank/master/contacts.csv') as f:
+with open(onlinecontacts) as f:
     reader = csv.reader(f)
     data = list(reader)
 
@@ -16,7 +19,7 @@ for i in data:
         count+=1
         comp = 100*count/(tot+1)
         int(i[1])
-        rc = subprocess.check_call(["https://raw.githubusercontent.com/alebovic/textbank/master/messagesapp.sh",i[1],"Hello, {name}! You have been bulk texted.".format(i[0])])
+        rc = subprocess.check_call([bashfile,i[1],"Hello, {name}! You have been bulk texted.".format(i[0])])
         print(str(comp) + "% COMPLETE (" + str(count) + "/" + str(tot+1) + ")")
     except:
         count+=1
