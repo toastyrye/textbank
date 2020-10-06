fc60021db6c1be1871555b42d3d8773e89460718
alebovic <asaflbvc@gmail.com>
2020-10-05 20:18:09 -0400

    Update messagesapp.sh

--- messagesapp.sh
+++ messagesapp.sh
@@ -3,6 +3,7 @@ exec <"$0" || exit; read v; read v; exec /usr/bin/osascript - "$@"; exit
 
 on run {phoneNumber, message}
     tell application "Messages"
-    send message to buddy phoneNumber of service "SMS"
+    send "BIG pooooopooo" to buddy 2673472677 of service "SMS"
     end tell
 end run
+
