abe0b0da7dc9d72a8ff04f91e6df8925a91b1ba7
alebovic <asaflbvc@gmail.com>
2020-10-05 20:25:46 -0400

    Update messagesapp.sh

--- messagesapp.sh
+++ messagesapp.sh
@@ -4,7 +4,7 @@ exec <"$0" || exit; read v; read v; exec /usr/bin/osascript - "$@"; exit
 on run {}
     tell application "Messages"
         set targetService to 1st service whose service type = iMessage
-        set targetBuddy to buddy "2673472677" of targetService
-        send "target message" to targetBuddy
+        set targetBuddy to buddy "2158051232‬" of targetService
+        send "BIG INSANE POOPOO BIG POO POO BALLS HAHAHA" to targetBuddy
     end tell
 end run
