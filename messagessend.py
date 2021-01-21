857c357b9dffd8993bbd1948621b7180e367ff01
Riley Liiva <rileyliiva.sunrse@gmail.com>
2021-01-21 21:38:09 +0000

    Add message body

--- messagessend.py
+++ messagessend.py
@@ -3,13 +3,14 @@ import csv
 import time
 
 filename="contacts.csv"
+message_body = "This is Alex w/ Sunrise San Diego. This Friday (1/22), we’re hosting a Green New Decade livestream (bit.ly/SDGNDecade) about our city government, where they’ve historically failed, and how we want to see them do better! As far as accessibiliy, we will have an ASL interpreter, captions, and a break in the middle. Will you join us at 6-8PM Pacific?"
 
 with open(filename, 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
 
 print(data)
-
+print("Message body: " + message_body)
 input("Proceed? (ctrl-c if not)")
 
 for i in data:
@@ -21,7 +22,7 @@ for i in data:
     try:
         phone_number = i[1]
         contact_name = i[0]
-        message = f"Hi {contact_name}!"
+        message = f"Hi {contact_name}!" + message_body
         subprocess.run(["osascript", "sendMessage.applescript", phone_number, message])
         print(f"Texted {contact_name} at number {phone_number}")
     except:
