f477bef31fda986b77c793489433c950a91274cf
toastyrye <47930737+toastyrye@users.noreply.github.com>
2021-01-21 21:02:15 -0800

    Increase time between texts

--- messagessend.py
+++ messagessend.py
@@ -5,7 +5,7 @@ import csv
 import time
 
 filename="contacts.csv"
-message_body = "This is Alex w/ Sunrise San Diego. This Friday (1/22), we are hosting a Green New Decade livestream (bit.ly/SDGNDecade) about our city government, where they have historically failed, and how we want to see them do better! As far as accessibiliy, we will have an ASL interpreter, captions, and a break in the middle. Will you join us at 6-8PM Pacific?"
+message_body = "This is Alex w/ Sunrise San Diego. This Friday (1/22), we are hosting a Green New Decade livestream (bit.ly/SDGNDecade) about our city government, where they have historically failed, and how we want to see them do better! As far as accessibility, we will have an ASL interpreter, captions, and a break in the middle. Will you join us at 6-8PM Pacific?"
 
 with open(filename, 'r') as f:
     reader = csv.reader(f)
@@ -19,7 +19,7 @@ for i in data:
     if i[0] == "Name":
         continue
 
-    time.sleep(3)
+    time.sleep(10)
     
     try:
         phone_number = i[1]
